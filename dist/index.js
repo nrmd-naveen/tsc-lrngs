581ca57794a05b5e287cfafeded713cd127317d3
nrmd-naveen <nrmdnaveen@gmail.com>
2025-01-13 23:52:34 +0530

    Prisma Class

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// First Postgress App
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,19 +9,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
-const connectionString = "postgresql://test_owner:2Obki0PejtNh@ep-bold-recipe-a5t4z5al.us-east-2.aws.neon.tech/test?sslmode=require";
-const pgClient2 = new pg_1.Client({
-    user: "test_owner",
-    password: "2Obki0PejtNh",
-    host: "ep-bold-recipe-a5t4z5al.us-east-2.aws.neon.tech",
-    port: 5432,
-    database: "test"
+const client_1 = require("@prisma/client");
+const client = new client_1.PrismaClient();
+const createUser = () => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield client.users.create({
+        data: {
+            username: "naveenrajan",
+            password: "adjbfidsfssdfnk"
+        },
+        select: {
+            username: true
+        }
+    });
+    console.log(user);
 });
-const pgClient = new pg_1.Client(connectionString);
-const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield pgClient.connect();
-    const res = yield pgClient.query("SELECT * FROM users;");
-    console.log(res);
-});
-main();
+createUser();
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-// First Postgress App
+import { PrismaClient } from "@prisma/client";
 
-import { Client } from 'pg'
+const client = new PrismaClient();
 
-const connectionString = "postgresql://test_owner:2Obki0PejtNh@ep-bold-recipe-a5t4z5al.us-east-2.aws.neon.tech/test?sslmode=require"
-
-const pgClient2 = new Client({
-    user: "test_owner",
-    password: "2Obki0PejtNh",
-    host: "ep-bold-recipe-a5t4z5al.us-east-2.aws.neon.tech",
-    port: 5432,
-    database: "test"
-})
-const pgClient = new Client(connectionString)
-
-const main = async () => {
-    await pgClient.connect()
-    const res = await pgClient.query("SELECT * FROM users;")
-    console.log(res)
+const createUser = async() => {
+    const user = await client.user.create({
+        data: {
+            username: "naveenrajan",
+            password: "adjbfidsfssdfnk"
+        },
+        select: {
+            username: true
+        }
+    })
+    console.log(user)
 }
-
-main()
+createUser()
